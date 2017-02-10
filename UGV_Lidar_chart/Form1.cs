2c6a57d4644f808107cad43153928ba357383202
Bobby Judd <bobbymjudd@gmail.com>
2017-02-10 14:53:04 -0800

    Restructured data members

--- UGV_Lidar_chart/Form1.cs
+++ UGV_Lidar_chart/Form1.cs
@@ -15,16 +15,22 @@ namespace UGV_Lidar_chart
 {
     public partial class Form1 : Form
     {
-
-        
-        
-        private int randomNum = 150;
         private List<long> distances = new List<long>();
+        private SerialPort urg;
+        private bool run;
+        private const int GET_NUM = 10;
+        private int start_step, end_step;
+        private string port_name;
+        private int baudrate;
 
         public Form1()
         {
             InitializeComponent();
 
+            //Start running by default
+            run = true;
+
+            //Set polar graph specifications
             chart1.Series["LidarData"].ChartType = SeriesChartType.Polar;
             chart1.Series["LidarData"].Color = Color.Red;
 
@@ -37,69 +43,64 @@ namespace UGV_Lidar_chart
             ax.Interval = 15;   // 15° interval
             ax.Crossing = -90;    // start the segments at the top!
 
-
+            //Open the serial connection to the lidar
+            port_name = "COM3";
+            baudrate = 9600;
+            urg = new SerialPort(port_name, baudrate);
+            urg.NewLine = "\n\n";
+            urg.Open();
+            urg.Write(SCIP_Writer.SCIP2());
+            urg.ReadLine(); // ignore echo back
+            
             Application.Idle += build_graph;
         }
 
         void build_graph(object sender, EventArgs e)
         {
             const int GET_NUM = 10;
-            const int start_step = 180; // 0 degrees. Consider getting info from a text box
-            const int end_step = 900;// 180 degrees. Consider getting info from a text box
-            try
+            start_step = 180; // 0 degrees. Consider getting info from a text box
+            end_step = 900;// 180 degrees. Consider getting info from a text box
+
+            //Get current lidar data
+            if (run)
             {
-                string port_name = "COM3";
-                int baudrate = 9600;
-
-                SerialPort urg = new SerialPort(port_name, baudrate);
-                urg.NewLine = "\n\n";
-
-                urg.Open();
-
-                urg.Write(SCIP_Writer.SCIP2());
-                urg.ReadLine(); // ignore echo back
-                urg.Write(SCIP_Writer.MD(start_step, end_step));
-                urg.ReadLine(); // ignore echo back
-
-                List<long> distances = new List<long>();
-                long time_stamp = 0;
-                for (int i = 0; i < GET_NUM; ++i)
+                try
                 {
-                    string receive_data = urg.ReadLine();
-                    if (!SCIP_Reader.MD(receive_data, ref time_stamp, ref distances))
+                    urg.Write(SCIP_Writer.MD(start_step, end_step));
+                    urg.ReadLine(); // ignore echo back
+
+                    List<long> distances = new List<long>();
+                    long time_stamp = 0;
+                    for (int i = 0; i < GET_NUM; ++i)
                     {
-                        Console.WriteLine(receive_data);
-                        break;
+                        string receive_data = urg.ReadLine();
+                        if (!SCIP_Reader.MD(receive_data, ref time_stamp, ref distances))
+                        {
+                            Console.WriteLine(receive_data);
+                            break;
+                        }
+                        if (distances.Count == 0)
+                        {
+                            Console.WriteLine(receive_data);
+                            continue;
+                        }
                     }
-                    if (distances.Count == 0)
-                    {
-                        Console.WriteLine(receive_data);
-                        continue;
-                    }
-                    // show distance data
-                    //Send the distances List to the form graph object
                 }
-
-                urg.Write(SCIP_Writer.QT()); // stop measurement mode
-                urg.ReadLine(); // ignore echo back
-                urg.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                chart1.Series["LidarData"].Points.Clear();
-
-                for (int i = 0; i < 180; i++)
+                catch (Exception ex)
                 {
-                    chart1.Series["LidarData"].Points.AddXY(i, distances[i]);
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    //Write lidar data to the polar graph
+                    chart1.Series["LidarData"].Points.Clear();
+
+                    for (int i = 0; i < 180; i++)
+                    {
+                        chart1.Series["LidarData"].Points.AddXY(i, distances[i]);
+                    }
                 }
             }
-
-
-
         }
 
         private void chart1_Click(object sender, EventArgs e)
@@ -109,7 +110,11 @@ namespace UGV_Lidar_chart
 
         private void button1_Click(object sender, EventArgs e)
         {
-            randomNum -= 10;
+            //Stop lidar measurement
+            run = false;
+            urg.Write(SCIP_Writer.QT()); // stop measurement mode
+            urg.ReadLine(); // ignore echo back
+            urg.Close();
         }
     }
 }
